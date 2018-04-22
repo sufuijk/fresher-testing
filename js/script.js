3151e2804c894d868467a29d052284370e339f8e
Sufuijk desktop <sufuijk@gmail.com>
2018-04-22 11:39:31 +0700

    fresher test

--- js/script.js
+++ js/script.js
@@ -39,12 +39,13 @@ $("#infoForm").submit(function(e){
 });
 
 
-	function validateEmail(email) {
-		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(email);
-	}
-	function validatePhone(numberPhone) {
-		var re = /^[0-9 ()-]+$/;
-		return re.test(numberPhone);
-	}
+function validateEmail(email) {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
+function validatePhone(numberPhone) {
+	var re = /^[0-9 ()-]+$/;
+	return re.test(numberPhone);
+}
 
